33ea8c86dc9523db5e2366ea19e09e80001b4410
RodrigoCastillo <rcastillol1601@alumno.ipn.mx>
2023-03-11 14:48:34 -0600

    edit

--- Servidor.java
+++ Servidor.java
@@ -3,15 +3,16 @@ import java.net.*;
 
 public class Servidor {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(5000);
-        System.out.println("Servidor iniciado en el puerto 5000...");
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+            System.out.println("Servidor iniciado en el puerto 5000...");
 
-        while (true) {
-            Socket socket = serverSocket.accept();
-            System.out.println("Cliente conectado: " + socket.getInetAddress().getHostName());
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Cliente conectado: " + socket.getInetAddress().getHostName());
 
-            HiloCliente hilo = new HiloCliente(socket);
-            hilo.start();
+                HiloCliente hilo = new HiloCliente(socket);
+                hilo.start();
+            }
         }
     }
 }
