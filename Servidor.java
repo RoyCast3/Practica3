b84659a5e30722c7bdf421406dd3fc2e2d1ba4fe
RodrigoCastillo <rcastillol1601@alumno.ipn.mx>
2023-03-11 20:12:49 -0600

    Edit

--- Servidor.java
+++ Servidor.java
@@ -3,8 +3,15 @@ import java.net.*;
 
 public class Servidor {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(5000);
-        System.out.println("Servidor iniciado en el puerto 5000...");
+
+        if (args.length != 1) {
+            System.err.println("Se requiere un puerto");
+            System.exit(1);
+        }
+        
+        int port = Integer.parseInt(args[0]);
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Servidor iniciado en el puerto "+ port);
 
         while (true) {
             Socket socket = serverSocket.accept();
@@ -12,20 +19,21 @@ public class Servidor {
 
             BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String mensaje;
+            int o1, o2;
             mensaje = entrada.readLine();
             int num1 = Integer.parseInt(mensaje);
             mensaje = entrada.readLine();
             int num2 = Integer.parseInt(mensaje);
+            mensaje = entrada.readLine();
+            o1 = Integer.parseInt(mensaje);
+            mensaje = entrada.readLine();
+            o2 = Integer.parseInt(mensaje);
 
-            HiloCliente hilo = new HiloCliente(socket, 1, num1, num2);
-            HiloCliente hilo2 = new HiloCliente(socket, 2, num1, num2);
-            HiloCliente hilo3 = new HiloCliente(socket, 3, num1, num2);
-            HiloCliente hilo4 = new HiloCliente(socket, 4, num1, num2);
+            HiloCliente hilo = new HiloCliente(socket, o1, num1, num2);
+            HiloCliente hilo2 = new HiloCliente(socket, o2, num1, num2);
 
             hilo.start();
             hilo2.start();
-            hilo3.start();
-            hilo4.start();
 
             mensaje = entrada.readLine();
             System.out.println(mensaje);
